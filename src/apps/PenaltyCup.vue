12a2f4faf4eaf228aea53b9cc72942a6ed7e1658
vinieFortes <vinie4apps@gmail.com>
2023-07-05 13:03:50 -0300

    penalty cup

--- src/apps/PenaltyCup.vue
+++ src/apps/PenaltyCup.vue
@@ -12,6 +12,12 @@ import {defineComponent, ref} from 'vue'
 
 export default defineComponent({
   name: 'PenaltyCup',
+  mounted() {
+    const iframe = document.createElement('iframe');
+    iframe.onload = function() { alert('myframe is loaded'); }; // before setting 'src'
+    iframe.src = '...';
+    document.body.appendChild(iframe); // add it to wherever you need it in the document
+  }
 
 })
 </script>
